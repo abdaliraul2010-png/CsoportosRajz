2adc2f240d532ae95aa8a178b6fb9bbcc9fa7ee8
andorvok <zakarias.andor.mc1@gmail.com>
2026-05-13 10:31:00 +0200

    Falomb

--- kozosrajz.py
+++ kozosrajz.py
@@ -1,2 +1,33 @@
+import turtle as t
+
+t.speed(0)
+t.color("green")
+t.fillcolor("green")
+
+
+t.penup()
+t.goto(-80, 150)
+t.pendown()
+t.begin_fill()
+t.circle(80)
+t.end_fill()
+
+t.penup()
+t.goto(0, 180)
+t.pendown()
+t.begin_fill()
+t.circle(80)
+t.end_fill()
+
+t.penup()
+t.goto(80, 150)
+t.pendown()
+t.begin_fill()
+t.circle(80)
+t.end_fill()
+
+t.done()
+
 #Zakariás Andor.
 #A fa Lombjai
+
