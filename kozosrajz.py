0fb0ffd62cab4295d765d625aa80e8f689a00369
Abdali_Raul <abdaliraul2010@gmail.com>
2026-05-13 10:31:03 +0200

    fa_torsze

--- kozosrajz.py
+++ kozosrajz.py
@@ -1,4 +1,22 @@
-# Abdaliraul: A Fa törzse
+import turtle
 
-#Zakariás Andor.
-#A fa Lombjai
+
+turtle.color("brown")
+turtle.begin_fill()
+i=0
+
+while i<2 :
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(300)
+    turtle.left(90)
+    i+=1
+turtle.forward(100)
+
+
+turtle.end_fill()
+
+turtle.done()
+
+
+# Abdaliraul: A Fa törzse
